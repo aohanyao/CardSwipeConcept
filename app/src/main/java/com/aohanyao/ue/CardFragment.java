985001d8fc82ea4521b9991b62e2b051b4485536
JJC <928692385@qq.com>
2017-09-27 18:42:22 +0800

    finish top

---
+++ app/src/main/java/com/aohanyao/ue/CardFragment.java
@@ -0,0 +1,44 @@
+package com.aohanyao.ue;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CardFragment extends Fragment {
+
+
+    public CardFragment() {
+        // Required empty public constructor
+    }
+
+    public static CardFragment newInstance(int color) {
+
+        Bundle args = new Bundle();
+        args.putInt("color", color);
+        CardFragment fragment = new CardFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_card, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getView().findViewById(R.id.ll_card_bg).setBackgroundColor(getArguments().getInt("color"));
+    }
+}
--- app/src/main/java/com/aohanyao/ue/MainActivity.java
+++ app/src/main/java/com/aohanyao/ue/MainActivity.java
@@ -1,13 +1,82 @@
 package com.aohanyao.ue;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vpCard;
+    private CircleIndicator indicator;
+    private int[] cardColors = {0xff1678c4, 0xffe6424a, 0xffb34dae, 0xffff8a2f};
+    private int[] bgColors = {0xff035a9e, 0xffba3139, 0xff8c4284, 0xffff6b21};
+    private View llbg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.content_main);
+
+        initView();
+        initCard();
     }
+
+    private void initCard() {
+        final List<Fragment> mFragment = new ArrayList<>();
+        mFragment.add(CardFragment.newInstance(cardColors[0]));
+        mFragment.add(CardFragment.newInstance(cardColors[1]));
+        mFragment.add(CardFragment.newInstance(cardColors[2]));
+        mFragment.add(CardFragment.newInstance(cardColors[3]));
+
+        vpCard.setPageMargin(40);
+        vpCard.setPageTransformer(true, new ScaleInAlphaTransformer());
+        vpCard.setOffscreenPageLimit(mFragment.size() * 2);
+        vpCard.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return mFragment.get(position);
+            }
+
+            @Override
+            public int getCount() {
+                return mFragment.size();
+            }
+        });
+
+        indicator.setViewPager(vpCard);
+
+        llbg.setBackgroundColor(bgColors[0]);
+
+        vpCard.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                llbg.setBackgroundColor(bgColors[position]);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private void initView() {
+        vpCard = (ViewPager) findViewById(R.id.vp_card);
+        indicator = (CircleIndicator) findViewById(R.id.indicator);
+        llbg = findViewById(R.id.ll_bg);
+    }
+
 }
